36fd8f641fed4d4da028fbe6126a37a693d8301e
bunnyxt <bunnyxt@outlook.com>
2020-10-20 00:56:31 +0800

    Add cors support.

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 // config in .env
 require('dotenv').config();
 
-// express and express-graphql server
+// express, middlewares and express-graphql server
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 
 // root ans schema
 const schema = require('./src/schema');
 const root = require('./src/root');
 
-// start server
+// create express app
 const app = express();
+
+// cors
+app.use(cors());
+
+// graphql
 app.use('/graphql', graphqlHTTP({
   schema,
   rootValue: root,
   graphiql: true,
 }));
+
+// start server
 const PORT = Number(process.env.PORT);
 if (!(PORT >= 1024 && PORT <= 65535)) {
   console.error(`Invalid port ${PORT} detected!`);
   process.exit(1);
 }
-app.listen(PORT);
-console.log(`GraphQL API server running at localhost:${PORT}/graphql`);
+app.listen(PORT, () => {
+  console.log(`GraphQL API server running at localhost:${PORT}/graphql`);
+});
