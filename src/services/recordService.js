8db39a54335285e6b26e4f778db6504d7627bef2
bunnyxt <bunnyxt@outlook.com>
2020-11-02 21:51:25 +0800

    Add addRecord function.

--- src/services/recordService.js
+++ src/services/recordService.js
@@ -6,6 +6,18 @@ async function getRecordsByBvid(bvid) {
   return results.map((record) => new Record(record));
 }
 
+async function addRecord(
+  bvid, threshold, preciseValue, time, timespan, srcType, srcAuthor, srcUrl, remark,
+) {
+  const result = await db.query(
+    'INSERT INTO `ms_record` (`bvid`, `threshold`, `precise_value`, `time`, `timespan`, '
+    + '`src_type`, `src_author`, `src_url`, `remark`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+    [bvid, threshold, preciseValue, time, timespan, srcType, srcAuthor, srcUrl, remark],
+  );
+  return result.length > 0 ? new Record(result[0]) : null;
+}
+
 module.exports = {
   getRecordsByBvid,
+  addRecord,
 };
