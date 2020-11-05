b69ef2fae298de761df1995e8426d1af4ececb31
bunnyxt <bunnyxt@outlook.com>
2020-11-05 14:30:16 +0800

    Add countLunarYear.

--- src/utils/index.js
+++ src/utils/index.js
@@ -26,8 +26,52 @@ function allUndefined(array) {
   return array.filter((x) => x !== undefined).length === 0;
 }
 
+/**
+ * Check lunar year.
+ * @param year
+ * @returns {boolean}
+ */
+function isLunarYear(year) {
+  if (year % 100 !== 0) {
+    if (year % 4 === 0) {
+      return true;
+    }
+  } else {
+    if (year % 400 === 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * Count how many Feb 29th passed between start ts and end ts.
+ * @param startTs
+ * @param endTs
+ * @returns {number}
+ */
+function countLunarYear(startTs, endTs) {
+  const startYear = new Date(startTs * 1000).getFullYear();
+  const endYear = new Date(endTs * 1000).getFullYear();
+  let currentYear = startYear;
+  let count = 0;
+  while (currentYear <= endYear) {
+    if (isLunarYear(currentYear)) {
+      const feb29StartDate = new Date(new Date().setFullYear(currentYear, 1, 29));
+      const feb29StartTs = Math.floor(feb29StartDate.setHours(0, 0, 0, 0) / 1000);
+      const feb29EndTS = feb29StartTs + 24 * 60 * 60;
+      if (startTs < feb29StartTs && endTs > feb29EndTS) {
+        count += 1;
+      }
+    }
+    currentYear += 1;
+  }
+  return count;
+}
+
 module.exports = {
   removeSecond,
   addPaddingZero,
   allUndefined,
+  countLunarYear,
 };
